9f8e4cd50574ee2b338a7782bbe45b69204318b8
kimyoungsun <kyssmart@naver.com>
2018-08-20 01:29:53 +0900

    1111
    111

---
+++ Assets/Scripts/CommonInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public delegate void VOID_FUN_VOID();
+public enum CharType { Player, Enemy }
+public enum AnimState 
+{
+	Idle = 1,
+	Move = 2,
+	Attack01 = 11,
+	Attack02 = 12,
+	Attack03 = 13,
+	Skill01	= 21,
+	Skill02 = 22
+}
+public class CommonInfo {
+	
+}
